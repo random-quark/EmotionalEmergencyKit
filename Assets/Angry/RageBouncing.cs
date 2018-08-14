b09873d87195bfb901f2fb775cd4d1d10d60320e
Tom Chambers <tom.chambers@gmail.com>
2018-08-14 16:53:05 +0100

    Merge

--- Assets/Angry/RageBouncing.cs
+++ Assets/Angry/RageBouncing.cs
@@ -8,6 +8,8 @@ public class RageBouncing : MonoBehaviour
     bool landed = false;
     Rigidbody rb;
 
+    float lastShakeTime;
+
     public Shake cameraShake;
 
     int fireDirection = -1;
@@ -28,12 +30,14 @@ public class RageBouncing : MonoBehaviour
         }
     }
 
+
     void OnCollisionEnter(Collision coll)
     {
-        
-        if (CheckWallCollision(coll) && timeSinceShake > 500)
+        float timeSinceShake = Time.time - lastShakeTime;    
+        if (CheckWallCollision(coll) && timeSinceShake > 1)
         {
             cameraShake.DoShake();
+            lastShakeTime = Time.time;
         }
 
 
@@ -51,7 +55,7 @@ public class RageBouncing : MonoBehaviour
     }
 
     bool CheckWallCollision(Collision coll) {
-        List<string> walls = new List<string> { "RightWall", "TopGate", "BottomGate" };
+        List<string> walls = new List<string> { "RightWall", "TopGate", "BottomGate", "Floor", "Ceiling" };
         bool result = walls.Contains(coll.gameObject.name);
         return result;
     }
@@ -60,10 +64,10 @@ public class RageBouncing : MonoBehaviour
     IEnumerator JumpAround() {
         while(true) {
             if (fireDirection > 0) {
-                rb.AddForce(new Vector3(1000, 500, 0));
+                rb.AddForce(new Vector3(1500, 500, 0));
                 fireDirection = -1;
             } else {
-                rb.AddForce(new Vector3(-1000, 500, 0));
+                rb.AddForce(new Vector3(-1500, 500, 0));
                 fireDirection = 1;
             }
             yield return new WaitForSeconds(Random.Range(0.8f, 1.6f));
