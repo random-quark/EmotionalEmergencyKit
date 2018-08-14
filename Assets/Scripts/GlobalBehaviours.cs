71c6398be867e45b729d73b3867edf171473b73d
Theo Papatheodorou <t.papatheodorou@gold.ac.uk>
2018-08-14 18:11:22 +0100

    added globab behaviour to angry scene

---
+++ Assets/Scripts/GlobalBehaviours.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobalBehaviours : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Restart"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if (Input.GetButtonDown("LoadScene1"))
+        {
+            SceneManager.LoadScene("fantastic");
+        }
+        else if (Input.GetButtonDown("LoadScene2"))
+        {
+            SceneManager.LoadScene("AngryScene");
+        }
+
+	}
+}
